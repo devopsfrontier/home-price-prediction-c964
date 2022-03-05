b1317a747b3c3e43271d3119efda1dcaeda5c830
devopsfrontier <39969509+devopsfrontier@users.noreply.github.com>
2022-03-05 08:54:18 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,21 +15,12 @@ def Home():
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
-    Fuel_Type_Diesel=0
     if request.method == 'POST':
-        Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        Year=2020-Year
+        Year = int(request.form['yr_built'])
+        Kms_Driven=int(request.form['yr_renovated'])
+        Owner=int(request.form['sqft_living'])
+        Fuel_Type_Petrol=request.form['sqft_lot']
+        /*
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
@@ -40,7 +31,8 @@ def predict():
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+            */
+        prediction=model.predict([[Kms_Driven,Owner,Year,Fuel_Type_Petrol]])
         output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
